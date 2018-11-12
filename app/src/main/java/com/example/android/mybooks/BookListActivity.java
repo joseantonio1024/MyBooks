c7633b6c13ea429175676d7b42063be99033feea
Jose Antonio <joseantonio1024@gmail.com>
2018-11-12 19:16:15 +0100

    CHANGE: se suprime la descripción del canal de notificaciones para simplificar el código

--- app/src/main/java/com/example/android/mybooks/BookListActivity.java
+++ app/src/main/java/com/example/android/mybooks/BookListActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.mybooks;
 
 import com.example.android.mybooks.model.BookContent;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -52,6 +56,7 @@ public class BookListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
 
+        createNotificationChannel();
         initToolbar();
         initFab();
         isTwoPane();
@@ -60,6 +65,19 @@ public class BookListActivity extends AppCompatActivity {
         registerSwipeRefreshLayout();
 
     }//End onCreate()
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = getString(R.string.default_notification_channel_id);
+            String channelName = getString(R.string.default_notification_channel_name);
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+            // Register the channel with the system
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 
     private void initToolbar(){
         Toolbar toolbar = findViewById(R.id.toolbar);
--- app/src/main/java/com/example/android/mybooks/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/android/mybooks/MyFirebaseMessagingService.java
@@ -1,19 +1,18 @@
 package com.example.android.mybooks;
 
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+// servicio iniciado automáticamente por el SDK de Firebase
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseMsgService";
@@ -53,11 +52,9 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      * @param messageBody FCM message body received.
      */
     private void sendNotification(String messageBody) {
-
         Intent intent = new Intent(this, BookListActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, getString(R.string.default_notification_channel_id))
@@ -70,12 +67,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        // Since android Oreo notification channel is needed.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String channelId = getString(R.string.default_notification_channel_id);
-            NotificationChannel channel = new NotificationChannel(channelId, getString(R.string.default_notification_channel_name), NotificationManager.IMPORTANCE_DEFAULT);
-            notificationManager.createNotificationChannel(channel);
-        }
         notificationManager.notify(0,notificationBuilder.build());
     }
 }
